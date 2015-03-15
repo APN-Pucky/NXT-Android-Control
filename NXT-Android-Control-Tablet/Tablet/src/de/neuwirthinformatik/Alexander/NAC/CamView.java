d226c58c9db608a0f1c36514e5cf8a6d24258fa6
APN-Pucky <APN-Pucky@users.noreply.github.com>
2015-03-15 16:18:20 +0100

    After Init

---
+++ NXT-Android-Control-Tablet/Tablet/src/de/neuwirthinformatik/Alexander/NAC/CamView.java
@@ -0,0 +1,14 @@
+package de.neuwirthinformatik.Alexander.NAC;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+public class CamView extends ImageView
+{
+
+	public CamView(Context context)
+	{
+		super(context);
+	}
+
+}
--- NXT-Android-Control-Tablet/Tablet/src/de/neuwirthinformatik/Alexander/NAC/MainActivity.java
+++ NXT-Android-Control-Tablet/Tablet/src/de/neuwirthinformatik/Alexander/NAC/MainActivity.java
@@ -1,13 +1,16 @@
 package de.neuwirthinformatik.Alexander.NAC;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 public class MainActivity extends Activity
 {
-
+	public static MainActivity _this;
+	public CamView camview;
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -16,5 +19,11 @@ public class MainActivity extends Activity
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
+		_this = this;
+		camview = new CamView(this);
+		FrameLayout preview = (FrameLayout) findViewById(R.id.frameLayout1);
+        preview.addView(camview);
+        camview.setImageDrawable(this.getWallpaper());
+        //init
 	}
 }
